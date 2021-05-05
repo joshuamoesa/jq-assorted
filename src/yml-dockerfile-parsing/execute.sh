4eaa7dae44223982bd0cc446327fc4346e4fdd4c
joshua.moesa@postnl.nl <joshua.moesa@postnl.nl>
2021-05-05 12:31:01 +0200

    Minor changes

--- src/yml-dockerfile-parsing/execute.sh
+++ src/yml-dockerfile-parsing/execute.sh
@@ -67,28 +67,38 @@ prepare_statement () {
     echo "Done"
 }
 
-get_aws_creds () {
-    tmp_role=$(echo cc-flow-$1-acc | cut -c1-60)
-    export temp_credentials=$(aws sts assume-role --role-arn $AWSTESTENVROLE --role-session-name $tmp_role)
-    export AWS_ACCESS_KEY_ID=$(echo ${temp_credentials} | jq -r '.Credentials.AccessKeyId')
-    export AWS_SESSION_TOKEN=$(echo ${temp_credentials} | jq -r '.Credentials.SessionToken')
-    export AWS_SECRET_ACCESS_KEY=$(echo ${temp_credentials} | jq -r ' .Credentials.SecretAccessKey')
-    export AWS_DEFAULT_REGION={{default_aws_region}}
-    eval "$(aws ecr get-login --no-include-email)" >/dev/null
-}
+get_aws_credentials () {
+    echo "Assuming role to $1"
+
+    unset TMP_ROLE_SESSION_NAME \
+            TEMP_CREDENTIALS \
+            AWS_ACCESS_KEY_ID \
+            AWS_SESSION_TOKEN \
+            AWS_SECRET_ACCESS_KEY \
+            AWS_DEFAULT_REGION
+
+    TMP_ROLE_SESSION_NAME=$(echo cc-flow-{{_comp.name}}-acc | cut -c1-60)
+
+    export TEMP_CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam::$1:role/$2-concourse-ci-operations --role-session-name $TMP_ROLE_SESSION_NAME)
+    export AWS_ACCESS_KEY_ID=$(echo ${TEMP_CREDENTIALS} | jq -r '.Credentials.AccessKeyId')
+    export AWS_SESSION_TOKEN=$(echo ${TEMP_CREDENTIALS} | jq -r '.Credentials.SessionToken')
+    export AWS_SECRET_ACCESS_KEY=$(echo ${TEMP_CREDENTIALS} | jq -r ' .Credentials.SecretAccessKey')
+    export AWS_DEFAULT_REGION={{ default_aws_region }}
+    }
 
 put_item () {
     # Determine prefix based on argument
-    if [[ $1 = "test" ]]
+    if [[ $1 = tst ]]
     then
-       AWSECSENVPREFIX="pnlt"
-    elif [[$1 = "acceptance"]]
+        AWSECSENVPREFIX="pnlt"
+    elif [[ $1 = acc ]]
     then
-       AWSECSENVPREFIX="pnla"
-    elif [[$1 = "production"]]
+        AWSECSENVPREFIX="pnla"
+    elif [[ $1 = prd ]]
     then
-       AWSECSENVPREFIX="pnlp"
+        AWSECSENVPREFIX="pnlp"
     fi
+
     TABLE=$AWSECSENVPREFIX"-esb-images"
     echo "Putting item in table" $TABLE
     if aws dynamodb list-tables | grep "$TABLE";
@@ -111,8 +121,8 @@ put_item () {
     echo "Done"
 }
 prepare_statement
-#get_aws_creds "test"
-put_item "test"
-#get_aws_creds "services"
+#get_aws_credentials {{ test_accountnr }} pnlt
+put_item tst
+#get_aws_credentials {{ services_accountnr }} pnlap
 #put_item "acceptance"
 #put_item "production"
